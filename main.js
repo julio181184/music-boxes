ae14d520e787223e1879f4a965319a6e89873c8e
julio181184 <julio181184@gmail.com>
2015-10-01 21:53:54 -0400

    ya

--- main.js
+++ main.js
@@ -8,39 +8,42 @@ $(document).ready(function(){
     var anote = document.getElementById("a-note");
     var bnote = document.getElementById("b-note");
     
-    $("#c").on("click",function(){
+
+    
+    $("#c").on("click mouseover",function(){
         cnote.currentTime = 0;
         cnote.play();
     });
     
-    $("#d").on("click",function(){
+    $("#d").on("click mouseover",function(){
         dnote.currentTime = 0;
         dnote.play();
     }); 
     
-    $("#e").on("click",function(){
+    $("#e").on("click mouseover",function(){
         enote.currentTime = 0;
         enote.play();
     });
     
-    $("#f").on("click",function(){
+    $("#f").on("click mouseover",function(){
         fnote.currentTime = 0;
         fnote.play();
     });
     
-    $("#g").on("click",function(){
+    $("#g").on("click mouseover",function(){
         gnote.currentTime = 0;
         gnote.play();
     });
     
-    $("#a").on("click",function(){
+    $("#a").on("click mouseover",function(){
         anote.currentTime = 0;
         anote.play();
     });
     
-    $("#b").on("click",function(){
+    $("#b").on("click mouseover",function(){
         bnote.currentTime = 0;
         bnote.play();
     });
     
+ 
 });
