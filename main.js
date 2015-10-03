1e6a8d1b96c0c75c4542b60261a1288509c25c90
julio181184 <julio181184@gmail.com>
2015-10-02 20:05:22 -0400

    con ecualizer

--- main.js
+++ main.js
@@ -91,5 +91,17 @@ $(document).ready(function(){
         
     });
     
-    
+    function fluctuate(bar) {
+    var hgt = Math.random() * 30;
+        hgt += 1;
+        var t = hgt * 30;
+        bar.animate({
+        height: hgt
+        }, t, function() {
+        fluctuate($(this));
+        });
+        }   
+        $(".bar").each(function(i) {
+        fluctuate($(this));
+        });
 });
